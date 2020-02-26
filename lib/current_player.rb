cd426e20be87b6a35e3918410a79d7e9e53b00ca
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-02-26 21:58:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,11 @@
 def turn_count(board)
   count = 0
-  counter = 0
-  board[counter].each do |cell|
+  board.each do |cell|
     if cell == "X"
       count +=1
     elsif cell == "O"
       count +=1
     end
-    counter +=1
   end
   return count
 end
